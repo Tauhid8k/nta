93502c629ef38f297d391ca359e6d55b64ed10b6
Tauhid8k <tauhid8k@gmail.com>
2023-05-30 18:06:38 +0600

    navbar, hero and partial category slider

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+  $(".category-slider").slick({
+    arrows: false,
+    dots: true,
+    speed: 300,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
